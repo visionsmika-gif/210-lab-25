dca8fe0e574b2382d58d799c7a11322782186ed0
visionsmika-gif <visionsmika@gmail.com>
2025-10-23 19:12:51 -0700

    update

--- main.cpp
+++ main.cpp
@@ -153,95 +153,95 @@ long long readSet(set<string>& mySet) {
 // Args:    a vector
 // Returns: time in microseconds
 long long sortVector(vector<string>& myVector) {
-    // Begin timing
-    auto start = high_resolution_clock::now();
-
-    // Sort vector
-    sort(myVector.begin(), myVector.end());
-
-    // End timing
-    auto end = high_resolution_clock::now();
-
-    // Return time result
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    sort(myVector.begin(), myVector.end());                     // Sort vector
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
 // Function to return the time it takes to sort a list.
 // Args:    a list
 // Returns: time in microseconds
 long long sortList(list<string>& myList) {
-    // Begin timing
-    auto start = high_resolution_clock::now();
-
-    // Sort list
-    myList.sort();
-
-    // End timing
-    auto end = high_resolution_clock::now();
-
-    // Return time result
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    myList.sort();                                              // Sort list
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
 // Function to return the time it takes to sort a set.
-// Args:    a vector
+// Args:    a set
 // Returns: -1 (because a set is already sorted)
 long long sortSet(set<string>& mySet) {
     return -1;
 }
 
+// Function to return the time it takes insert an element in the middle of a vector.
+// Args:    a vector
+// Returns: time in microseconds
 long long insertVector(vector<string>& myVector) {
-    auto start = high_resolution_clock::now();
-    auto middleIt = myVector.begin() + (myVector.size() / 2);
-
-    myVector.insert(middleIt, INSERT_VALUE);
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    auto middleIt = myVector.begin() + (myVector.size() / 2);   // Create an iterator in the middle
+    myVector.insert(middleIt, INSERT_VALUE);                    // Insert a value in the middle
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
+// Function to return the time it takes insert an element in the middle of a list.
+// Args:    a list
+// Returns: time in microseconds
 long long insertList(list<string>& myList) {
-    auto start = high_resolution_clock::now();
-    auto middleIt = myList.begin();
-    advance(middleIt, (myList.size() / 2)); // Use advance because list is not contiguous
-
-    myList.insert(middleIt, INSERT_VALUE);
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    auto middleIt = myList.begin();                             // Create an iterator
+    advance(middleIt, (myList.size() / 2));                     // Advance the iterator to the middle
+    myList.insert(middleIt, INSERT_VALUE);                      // Insert a value in the middle
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
+
+// Function to return the time it takes insert an element in a set.
+// Args:    a set
+// Returns: time in microseconds
 long long insertSet(set<string>& mySet) {
-    auto start = high_resolution_clock::now();
-    mySet.insert(INSERT_VALUE); // not sequenced
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    mySet.insert(INSERT_VALUE);                                 // Insert a value
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
+// Function to return the time it takes delete an element in the middle of a vector.
+// Args:    a vector
+// Returns: time in microseconds
 long long deleteVector(vector<string>& myVector) {
-    auto start = high_resolution_clock::now();
-    auto middleIt = myVector.begin() + (myVector.size() / 2);
-
-    myVector.erase(middleIt);
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    auto middleIt = myVector.begin() + (myVector.size() / 2);   // Create an iterator in the middle
+    myVector.erase(middleIt);                                   // Delete a value in the middle
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
+// Function to return the time it takes delete an element in the middle of a list.
+// Args:    a list
+// Returns: time in microseconds
 long long deleteList(list<string>& myList) {
-    auto start = high_resolution_clock::now();
-    auto middleIt = myList.begin();
-    advance(middleIt, (myList.size() / 2)); // Use advance because list is not contiguous
-
-    myList.erase(middleIt);
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    auto middleIt = myList.begin();                             // Create an iterator
+    advance(middleIt, (myList.size() / 2));                     // Advance the iterator to the middle
+    myList.erase(middleIt);                                     // Delete a value in the middle
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
+// Function to return the time it takes delete an element in the middle of a set.
+// Args:    a set
+// Returns: time in microseconds
 long long deleteSet(set<string>& mySet) {
-    auto start = high_resolution_clock::now();
-    auto middleIt = mySet.begin();
-    advance(middleIt, (mySet.size() / 2)); // Use advance because list is not contiguous
-
-    mySet.erase(middleIt);
-    auto end = high_resolution_clock::now();
-    return (duration_cast<microseconds>(end - start)).count();
+    auto start = high_resolution_clock::now();                  // Begin timing
+    auto middleIt = mySet.begin();                              // Create an iterator
+    advance(middleIt, (mySet.size() / 2));                      // Advance the iterator to the middle
+    mySet.erase(middleIt);                                      // Delete a value in the middle
+    auto end = high_resolution_clock::now();                    // End timing
+    return (duration_cast<microseconds>(end - start)).count();  // Return time result
 }
 
 /* syntax examples:
